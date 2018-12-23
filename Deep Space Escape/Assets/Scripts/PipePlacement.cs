62d8936bad9ddd48fdc86a2aa8ebb71227227d48
MatthewGaby <pixelball1213@gmail.com>
2018-12-23 10:53:22 -0500

    Pipe Placement Branch
    This is for placing pipes and stuff, still wip

---
+++ Deep Space Escape/Assets/Scripts/PipePlacement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePlacement : MonoBehaviour {
+    private bool isPlaced = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    bool getPlaced()
+    {
+        return isPlaced;
+    }
+    void Place()
+    {
+        isPlaced = true;
+    }
+    void Pickup()
+    {
+        isPlaced = false;
+    }
+}
