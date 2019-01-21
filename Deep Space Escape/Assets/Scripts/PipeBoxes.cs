b4fd6638d65180cddd3c934d407432f476f36386
Doug Guzman <dgguzman@mail.bradley.edu>
2019-01-20 18:57:48 -0600

    Merges PipeBoxes

--- Deep Space Escape/Assets/Scripts/PipeBoxes.cs
+++ Deep Space Escape/Assets/Scripts/PipeBoxes.cs
@@ -1,23 +1,51 @@
 ﻿using UnityEngine;
-using Meta;
+using Meta.HandInput;
 
-public class PipeBoxes : MonoBehaviour
+public class PipeBox : MonoBehaviour
 {
+    // Reference for the pipe type.
     public GameObject pipe;
+    [Space(20)]
+    public HandTrigger boxTrigger;
+    [Space(20)]
+    public Transform pipeSpawnLocation;
+    [Space(35)]
+    public bool spawnSwitch = false;//makes it so that pipes dont spawn like crazy
 
-	void Start ()
+    private void Start()
     {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
+        boxTrigger = gameObject.GetComponent<HandTrigger>();
+        if(boxTrigger == null)
+        {
+            Debug.Log(gameObject.name + " is missing a HandTrigger!");
+        }
+    }
 
-    void grabPipe()
+    public void Update()
     {
-        GrabEvent GetGrabEvent = new GrabEvent();
+        if (boxTrigger.HandCount > 0 && !spawnSwitch)
+        {
+            spawnSwitch = true;
+            Instantiate(pipe, transform.position, Quaternion.identity);
+        }
+        if (boxTrigger.HandCount <= 0)
+        {
+            spawnSwitch = false;
+        }
+    }
+
+    // Trigger for when the player's hands exit the box.
+    //void OnTriggerEnter(Collider other)
+    {
+       /* if (other.tag == "Player")
+        {
+            PalmStateMachine currentPalm = new PalmStateMachine();
+            if (currentPalm.CurrentState == PalmState.Grabbing)
+            {
+                Instantiate(pipe, other.transform.position, Quaternion.identity);
+            }
+        }*/
+
         
     }
 }
